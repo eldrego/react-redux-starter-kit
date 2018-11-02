63da052ee98b996d041905deb98fc6ff8917949b
Mark Edomwande <mark.edomwande@andela.com>
2018-11-02 16:15:07 +0100

    git cache cleared

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import routes from './routes';
 require('dotenv').config();
 
 const debug = util.debuglog('server');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.server = http.createServer(app);
